dc2a886b0c1255d661669553b21d97f4e5efb16a
priprii <41253973+priprii@users.noreply.github.com>
2021-05-13 12:10:42 +0100

    Added support for S_DECREASE_COOLTIME_SKILL

--- ui/ui.js
+++ ui/ui.js
@@ -14,9 +14,16 @@ document.addEventListener('DOMContentLoaded', () => {
 	});
 	
 	mod.on('UI_SKILL_COOLDOWN', ({skillId, cooldown, useTime}) => {
-		var dss = document.querySelectorAll(`[data-skill='${skillId}']`);
-		dss.forEach(ds => {
-			activateSkill(ds, cooldown, useTime);
+		var dataSkills = document.querySelectorAll(`[data-skill='${skillId}']`);
+		dataSkills.forEach(dataSkill => {
+			activateSkill(dataSkill, cooldown, useTime);
+		});
+	});
+	
+	mod.on('UI_SKILL_COOLDOWN_UPDATE', ({skillId, cdRemaining}) => {
+		var dataSkills = document.querySelectorAll(`[data-skill='${skillId}']`);
+		dataSkills.forEach(dataSkill => {
+			updateSkill(dataSkill, cdRemaining);
 		});
 	});
 	
@@ -44,7 +51,7 @@ document.addEventListener('DOMContentLoaded', () => {
 						nodes[i] = document.createElement('tr');
 					}
 					var skillGroup = skillGroups[i][n];
-					nodes[i].innerHTML += `<td style="padding: ${configSettings.iconPadding}; opacity: ${isVisible ? configSettings.rOpacity / 100 : 0};"><div id="${skillGroup.skillId}" class="skill" data-skill="${skillGroup.skillId}" style="background-image: url('skills/${skillGroup.skillIcon}.png'); width: ${configSettings.iconSize}; height: ${configSettings.iconSize}; line-height: ${configSettings.iconSize};"></div></td>`;
+					nodes[i].innerHTML += `<td style="padding: ${configSettings.iconPadding}; opacity: ${isVisible ? configSettings.rOpacity / 100 : 0};"><div id="${skillGroup.skillId}" class="skill" data-skill="${skillGroup.skillId}" data-cooldown="" data-remaining="" style="background-image: url('skills/${skillGroup.skillIcon}.png'); width: ${configSettings.iconSize}; height: ${configSettings.iconSize}; line-height: ${configSettings.iconSize};"></div></td>`;
 				}
 			}
 			for(var m = 0; m < nodes.length; m++) {
@@ -55,20 +62,32 @@ document.addEventListener('DOMContentLoaded', () => {
 	
 	function activateSkill(target, cooldown, useTime) {
 		target.parentNode.opacity = `${configSettings.cdOpacity / 100}`;
-		var size = target.style.width;
 		target.style.setProperty('--time-left', '100%');
-		target.style.lineHeight = `${size}`;
+		target.setAttribute('data-cooldown', `${cooldown}`);
+		target.style.lineHeight = target.style.width;
 		
 		const cdInterval = setInterval(() => {
+			cooldown = parseInt(target.getAttribute('data-cooldown'), 10);
 			var cdRemaining = cdTimeRemaining(useTime, cooldown);
+			target.setAttribute('data-remaining', `${cdRemaining}`);
+			
 			target.style.setProperty('--time-left', `${cdRemaining / cooldown * 100}%`);
-			target.setAttribute('data-text', (cdRemaining / 1000).toFixed(1));
+			target.setAttribute('data-text', `${(cdRemaining / 1000).toFixed(1)}`);
 			if(cdRemaining <= 0) {
 				target.parentNode.opacity = `${configSettings.rOpacity / 100}`;
 				target.setAttribute('data-text', '');
 				clearInterval(cdInterval);
 			}
 		}, 1000 / 60);
+	}
+	
+	function updateSkill(target, newRemaining) {
+		var currentRemaining = parseInt(target.getAttribute('data-remaining'), 10);
+		var diff = currentRemaining - newRemaining;
+		target.setAttribute('data-remaining', `${newRemaining > 0 ? newRemaining : 0}`);
+		
+		var currentCooldown = parseInt(target.getAttribute('data-cooldown'), 10);
+		target.setAttribute('data-cooldown', `${currentCooldown - diff}`);
 	}
 	
 	function cdTimeRemaining(lastUseTime, cdDuration) {
